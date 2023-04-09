60ec5d1ee37b18d68b1bc8c604a3c16ebd3d7544
Jitendra virkayade <virkayadejitendra@gmail.com>
2023-04-09 15:44:12 +0530

    Added OccupationController

---
+++ InsurenceAPI/Controllers/OccupationController.cs
@@ -0,0 +1,30 @@
+﻿using InsurenceAPI.Data;
+using InsurenceAPI.DTOs;
+using InsurenceAPI.Errors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace InsurenceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OccupationController : ControllerBase
+    {
+        private DataContext _context { get; }
+
+        public OccupationController(DataContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetOccupations()
+        {
+            var result =  await this._context.Occupations.Select(occ=>occ.Name).ToListAsync();
+
+            return Ok(result);
+        }
+
+    }
+}
